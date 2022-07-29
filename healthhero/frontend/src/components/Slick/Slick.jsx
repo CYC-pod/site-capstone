a68489695adc1938efbf4cee16bceb32090f59d5
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-07-29 12:24:22 -0700

    issues w load

--- healthhero/frontend/src/components/Slick/Slick.jsx
+++ healthhero/frontend/src/components/Slick/Slick.jsx
@@ -1,16 +1,11 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { useState } from "react";
+
 import "../Slick/Slick.css";
-import { HSSU } from "../../constants";
-import { USC } from "../../constants";
-import { HU } from "../../constants";
-import { washu } from "../../constants";
-import { VT } from "../../constants";
-import { USF } from "../../constants";
-import { UTEP } from "../../constants";
+import { HSSU, USC, HU, washu, VT, USF, UTEP } from "../../constants";
 
 export default function Slick() {
   <script
@@ -18,6 +13,7 @@ export default function Slick() {
     crossorigin="anonymous"
   ></script>;
   const [sliderRef, setSliderRef] = useState(null);
+
   var settings = {
     dots: true,
     infinite: true,
@@ -26,6 +22,31 @@ export default function Slick() {
     slidesToScroll: 1,
     arrows: false,
   };
+
+  function cacheImages(array) {
+    if (!cacheImages.list) {
+      cacheImages.list = [];
+    }
+    var list = cacheImages.list;
+    for (var i = 0; i < array.length; i++) {
+      var img = new Image();
+      img.onload = function () {
+        var index = list.indexOf(this);
+        if (index !== -1) {
+          // remove image from the array once it's loaded
+          // for memory consumption reasons
+          list.splice(index, 1);
+        }
+      };
+      list.push(img);
+      img.src = array[i];
+    }
+  }
+
+  useEffect(() => {
+    cacheImages([HSSU, USC, HU, washu, VT, USF, UTEP]);
+  }, []);
+
   return (
     <div className="content">
       <div className="leftbtton">
@@ -37,7 +58,16 @@ export default function Slick() {
       <div className="slider">
         <Slider ref={setSliderRef} {...settings}>
           <div id="schoolHome">
-            <img src={USC} alt="USC" />
+            <img
+              src={USC}
+              onLoad={() => {
+                console.log("loaded");
+              }}
+              onError={(e) => {
+                console.log("on error", e);
+              }}
+              alt="USC"
+            />
           </div>
           <div id="schoolHome">
             <img src={HU} alt="Howard" />
