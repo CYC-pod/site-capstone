4122faf292a5a9103b594305a6c9ce3a1a811c56
yasminsenior02 <yasminsenior02@gmail.com>
2022-08-01 12:00:28 -0700

    soem changes

--- healthhero/frontend/src/components/SelComm/SelComm.jsx
+++ healthhero/frontend/src/components/SelComm/SelComm.jsx
@@ -1,12 +1,30 @@
 import * as React from "react";
-
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useAuthContext } from "../../../AuthContext/auth";
+import { CommForm } from "../CommForm/Commform";
 import "./SelComm.css";
 
 export default function SelComm() {
+  const buttonClickedHandler = () => {
+    console.log("You have been clicked a button!");
+  };
   return (
     <div>
       <div id="greenb">
-        <h1>Select your comm</h1>
+        <h1>Select your Community</h1>
+        <View div="screen">
+          <TouchableOpacity onPress={buttonClickedHandler} div="roundButton1">
+            <Text>example community</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={buttonClickedHandler}
+            style={styles.roundButton2}
+          >
+            <Text>I'm another button</Text>
+          </TouchableOpacity>
+        </View>
+        );
       </div>
     </div>
   );
