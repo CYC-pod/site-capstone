31eca004898779e56e993ab1e68b2a8cd3d7b277
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-08-03 14:43:40 -0700

    pre resp edits

--- healthhero/frontend/src/components/ResResults/ResCard.jsx
+++ healthhero/frontend/src/components/ResResults/ResCard.jsx
@@ -2,19 +2,21 @@ import "../ResResults/ResCard.css";
 import React from "react";
 
 export default function ResCard({ res }) {
+  const { id, name, image, location, description } = res; //containing them to contain these 3 keys
+
   return (
     <div className="RestCard">
       <div className="media">
         {res.image ? (
-          <img src={res.image} alt="product cover" />
+          <img src={image} alt="product cover" />
         ) : //   <img src={codepath} alt="product cover" /> put our own placeholder img here
         null}
       </div>
       <div className="res-info">
         <div className="info">
-          <p className="res-name">{res}</p>
-          <p className="res-loc">{res.location}</p>
-          <p className="res-des">{res.description}</p>
+          <h3 className="res-name">{name}</h3>
+          <p className="res-loc">{location}</p>
+          <p className="res-des">{description}</p>
         </div>
       </div>
     </div>
