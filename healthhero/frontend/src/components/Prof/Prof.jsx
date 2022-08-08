3fbff167f8406888fb46fc732382ab76d0617c68
Courtney Fenderson <cfenderson@cfender-ltmv2wd.internal.salesforce.com>
2022-08-08 16:24:18 -0700

    proff

--- healthhero/frontend/src/components/Prof/Prof.jsx
+++ healthhero/frontend/src/components/Prof/Prof.jsx
@@ -1,8 +1,18 @@
 import * as React from "react";
-
+import { AuthContextProvider, useAuthContext } from "../../../AuthContext/auth";
 import "../Prof/Prof.css";
 
 export default function Prof() {
+  const { user, setUser } = useAuthContext();
+  var email;
+  var username;
+  React.useEffect(() => {
+    console.log("user in prof :", user);
+    // console.log("user email: ", user.email);
+    email = user.email;
+    username = user.username;
+  }, [user]);
+  // setUser(res.data.user);
   return (
     <div>
       <div className="backgroundP">
@@ -10,7 +20,10 @@ export default function Prof() {
           <p>Your Profile</p>
         </div>
         <div>
-          <div id="round"> Name: School: Email: </div>
+          <div id="round">
+            Name: {username} School: Email: {email}
+          </div>
+          <div id="round"></div>
         </div>
       </div>
     </div>
