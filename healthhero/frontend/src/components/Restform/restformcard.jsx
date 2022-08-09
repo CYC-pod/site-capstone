9077e28a3b7348356cda38c69ca572d6158980a5
yasminsenior02 <yasminsenior02@gmail.com>
2022-08-09 15:00:50 -0700

    pushing

--- healthhero/frontend/src/components/Restform/restformcard.jsx
+++ healthhero/frontend/src/components/Restform/restformcard.jsx
@@ -9,28 +9,31 @@ export default function ResFormCard({ resId }) {
   useEffect(() => {
     async function getResId() {
       const res = await apiClient.listRestsbyId();
-    //   console.log(res);
-      console.log("restaurant list", res.data.restaurants)
+      //   console.log(res);
+      console.log("restaurant list", res.data.restaurants);
       setRestaurant(res.data.restaurants[0]);
-      ;
     }
     getResId();
   }, []);
-//const { image_url, name, location, description, restrictions } = resId;
 
-//   useEffect(() => {
-//     console.log("communities by school:", communities);
-//   }, [communities]);
+  useEffect(() => {
+    console.log(restaurant.restrictions);
+  }, [restaurant.restrictions]);
+  //const { image_url, name, location, description, restrictions } = resId;
+
+  //   useEffect(() => {
+  //     console.log("communities by school:", communities);
+  //   }, [communities]);
 
   return (
     <div className="ResCard">
       {/* <Link to={"/resDescript/" + id} className="media"> */}
-      <img className="resImage" src={restaurant.image}></img>
+      <img className="resImage" src={restaurant.image_url}></img>
       {/* </Link> */}
       <div className="resinfo">
         <p className="resname">{restaurant.name}</p>
         <p className="location">{restaurant.location}</p>
-        <p className="restrict">{restaurant.restrictions}</p>
+        <p className="restrict">{restaurant.restrictions} </p>
         <p className="resdes">{restaurant.description}</p>
       </div>
     </div>
