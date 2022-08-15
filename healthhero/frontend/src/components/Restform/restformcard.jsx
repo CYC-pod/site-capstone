dbc528bba7dbf07c712efc80d305a0c5a4137881
yasminsenior02 <yasminsenior02@gmail.com>
2022-08-14 20:34:49 -0700

    update

--- healthhero/frontend/src/components/Restform/restformcard.jsx
+++ healthhero/frontend/src/components/Restform/restformcard.jsx
@@ -1,11 +1,14 @@
-import "../ResResults/ResCard.css";
+import "./restformcard.css";
 import React from "react";
 import { useState, useEffect } from "react";
 import apiClient from "../../../services/apiClient";
+import { Box } from "@mui/system";
+import { Container } from "@mui/system";
+import { colors } from "@mui/material";
 
 export default function ResFormCard({ resId }) {
   const [restaurant, setRestaurant] = useState([]);
-  const [ restrictions, setRestrictions] = useState([])
+  const [restrictions, setRestrictions] = useState([]);
 
   useEffect(() => {
     async function getResId() {
@@ -23,8 +26,7 @@ export default function ResFormCard({ resId }) {
 
   //const { image_url, name, location, description, restrictions } = resId;
 
-
-    useEffect(() => {
+  useEffect(() => {
     async function getRestrictions() {
       console.log("restaurant id in getRest", restaurant.id);
       const res = await apiClient.listRestaurantRestrictions(restaurant.id);
@@ -34,27 +36,40 @@ export default function ResFormCard({ resId }) {
     getRestrictions();
   }, [restaurant]);
 
-
   useEffect(() => {
     console.log("restaurant's restrictions:", restrictions);
   }, [restrictions]);
 
-
   return (
     <div className="ResCard">
+      <Box sx={{ textAlign: "center", fontCol: "white" }}>
+        <p className="resname">{restaurant.name}</p>
+      </Box>
       {/* <Link to={"/resDescript/" + id} className="media"> */}
       <img className="resImage" src={restaurant.image_url}></img>
       {/* </Link> */}
       <div className="resinfo">
-        <p className="resname">{restaurant.name}</p>
-        <p className="location">{restaurant.location}</p>
-        {restrictions.map((restriction) => {
-        return (
-          <div> {restriction} </div>
-        );
-      })}
-        <p className="restrict">{restaurant.restrictions} </p>
-        <p className="resdes">{restaurant.description}</p>
+        <Container
+          sx={{
+            background: "#B1907F",
+            justifyContent: "space-between",
+            Height: "500%",
+            Width: "20",
+            float: "left",
+          }}
+        >
+          <Box sx={{ fontSize: "150%", justifyContent: "space-between" }}>
+            <p className="location">{restaurant.location}</p>
+            Restrictions:
+            {restrictions.map((restriction) => {
+              return <div> {restriction} </div>;
+            })}
+            <p className="restrict">{restaurant.restrictions} </p>
+          </Box>
+          <Box sx={{ fontSize: "150%", justifyContent: "space-between" }}>
+            <p className="resdes">{restaurant.description}</p>
+          </Box>
+        </Container>
       </div>
     </div>
   );
